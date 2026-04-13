e8a0f72e00fe54e6528f4d0398d29d80ec83ba6f
Daniel Rodriguez <dangelsfocus@gmail.com>
2026-04-13 15:31:39 -0500

    Init project

---
+++ src/test/java/com/project/franchise/FranchiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.franchise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FranchiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
